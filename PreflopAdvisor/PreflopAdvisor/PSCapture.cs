fbba4e49851775152b35580f3cc5036afdb66dcd
bloody snow <bloodysnowx@gmail.com>
2019-07-20 10:25:57 +0900

    create the solution

---
+++ PreflopAdvisor/PreflopAdvisor/PSCapture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PreflopAdvisor
+{
+    class PSCapture
+    {
+        protected IntPtr handle;
+
+        public PSCapture(string processName)
+        {
+            this.handle = Process.GetProcessesByName(processName).Select(p => p.MainWindowHandle).First(p => p != IntPtr.Zero);
+        }
+
+        public void Restore()
+        {
+            if (NativeCall.IsIconic(this.handle))
+                NativeCall.ShowWindow(this.handle, NativeCall.WindowShowStyle.Restore);
+        }
+    }
+}
